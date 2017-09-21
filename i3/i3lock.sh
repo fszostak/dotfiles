16320ce229ad33ea8613da1d2059f56a821e17b1
Gustavo Tonietto <tonietto@gmail.com>
2017-09-21 14:11:36 -0300

    Update i3lock

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -7,38 +7,39 @@ T=$D	       # text
 W='#ff5555ff'  # wrong
 V='#282a36ff'  # verifying
 
-i3lock -ef            \
+i3lock -ef                \
 \
---insidevercolor=$C   \
---ringvercolor=$D     \
+--insidevercolor=$C       \
+--ringvercolor=$D         \
 \
---insidewrongcolor=$C \
---ringwrongcolor=$W   \
+--insidewrongcolor=$C     \
+--ringwrongcolor=$W       \
 \
---insidecolor=$B      \
---ringcolor=$D        \
---linecolor=$B        \
---separatorcolor=$D   \
+--insidecolor=$B          \
+--ringcolor=$D            \
+--linecolor=$B            \
+--separatorcolor=$D       \
 \
---textcolor=$T        \
---timecolor=$T        \
---datecolor=$T        \
---keyhlcolor=$V       \
---bshlcolor=$W        \
+--textcolor=$T            \
+--timecolor=$T            \
+--datecolor=$T            \
+--keyhlcolor=$V           \
+--bshlcolor=$W            \
 \
---screen 0            \
---blur 10             \
---clock               \
---indicator           \
---timestr="%H:%M:%S"  \
---datestr="%A, %m %d %Y" \
+--screen 0                \
+--blur 10                 \
+--clock                   \
+--indicator               \
+--timestr="%H:%M:%S"      \
+--datestr="%A, %m %d %Y"  \
 \
---timefont=hack       \
---datefont=hack       \
-
-
-# --veriftext="Drinking verification can..."
-# --wrongtext="Nope!"
-# --textsize=20
-# --modsize=10
-# etc
+--timefont=hack           \
+--datefont=hack           \
+\
+--veriftext="Wait for it" \
+\
+--wrongtext="Duh!"        \
+\
+--textsize=30		  \
+\
+--modsize=15		  \
