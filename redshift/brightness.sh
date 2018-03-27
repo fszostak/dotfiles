bb4bbac7ec6d41f7263643ee167b927a619c4c7b
Gustavo Tonietto <tonietto@gmail.com>
2018-03-27 18:26:15 -0300

    Fix redshift indentation

--- redshift/brightness.sh
+++ redshift/brightness.sh
@@ -11,17 +11,17 @@ brightness_night="65"
 fade_time=60000
 
 case $1 in
-	period-changed)
-		case $3 in
-			night)
-				xbacklight -set $brightness_night -time $fade_time
-				;;
-			transition)
-				xbacklight -set $brightness_transition -time $fade_time
-				;;
-			daytime)
-				xbacklight -set $brightness_day -time $fade_time
-				;;
-		esac
-		;;
+  period-changed)
+    case $3 in
+      night)
+	xbacklight -set $brightness_night -time $fade_time
+	;;
+      transition)
+	xbacklight -set $brightness_transition -time $fade_time
+	;;
+      daytime)
+	xbacklight -set $brightness_day -time $fade_time
+	;;
+    esac
+    ;;
 esac
